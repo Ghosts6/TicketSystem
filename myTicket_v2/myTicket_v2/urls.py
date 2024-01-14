6b0cc9d9830bc187fb45ec5f5cdfe0c325abc2f1
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-01-14 22:21:33 +0000

    Update urls.py

--- myTicket_v2/myTicket_v2/urls.py
+++ myTicket_v2/myTicket_v2/urls.py
@@ -1,1 +1,10 @@
 
+from django.contrib import admin
+from django.urls import include,path
+
+urlpatterns = [
+    path('', include('Ticket.urls')),
+    path('admin/', admin.site.urls),
+]
+handler404 = 'myTicket_v2.views.custom_page_not_found'
+handler500 = 'myTicket_v2.views.custom_server_error'
